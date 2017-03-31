b3d50c54aea93a057e573d7c8caf707b1af6c6e5
Ales Galuska <ales.galuska@seznam.cz>
2017-03-31 12:38:53 +0200

    Add project files.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo1 = 5;
+            int cislo2 = 10;
+            int vysledek;
+            vysledek = cislo2 + cislo1;
+            // komentar v programu int cislo3 = 15
+
+            Console.Out.WriteLine(vysledek);
+            Console.ReadLine();
+
+        }
+    }
+}
